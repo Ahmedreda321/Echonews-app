02c47d73f6f0a416d0588f13a7168caaee85e965
ahmedreda321 <ahmedredahmedd@gmail.com>
2025-07-18 15:09:28 +0300

    refactor: Clean up NewsRepo constructor and improve error handling in getNews method

--- lib/features/home/data/repositories/news_repo.dart
+++ lib/features/home/data/repositories/news_repo.dart
@@ -1,27 +1,28 @@
 
 import 'package:dartz/dartz.dart';
+import 'package:news/core/network/error_handeler.dart';
 import 'package:news/features/home/data/mapper/news_mapper.dart';
 
 import '../../../../core/errors/failures.dart';
 import '../../../../core/network/network_info.dart';
 import '../../presentation/models/news.dart';
 import '../datasources/news_remote_data_source.dart';
-
 class NewsRepo {
   final NewsRemoteDataSource newsRemoteDataSource;
   final NetworkInfo networkInfo;
-  NewsRepo (this.newsRemoteDataSource, this.networkInfo);
+  
+  NewsRepo(this.newsRemoteDataSource, this.networkInfo);
 
-  Future <Either <Failure, List<News>>> getNews(String query) async {
-    if (await networkInfo.isConnected) {
-      try {
-        final news = await newsRemoteDataSource.getNews(query);
-        return Right(news.articles.toNewsList());
-      } catch (e) {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
+  Future<Either<Failure, List<News>>> getNews(String query) async {
+    if (!await networkInfo.isConnected) {
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
+    }
+
+    try {
+      final news = await newsRemoteDataSource.getNews(query);
+      return Right(news.articles.toNewsList());
+    } catch (error) {
+      return Left(ErrorHandler.handle(error).failure);
     }
   }
 }
